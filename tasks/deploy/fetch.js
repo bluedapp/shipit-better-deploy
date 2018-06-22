f1ba5db9e69a03765182e7998bb1d00fabdb8a85
jiashunming <jiashunming@outlook.com>
2018-06-22 20:04:09 +0800

    update: 100% compatible with older

--- tasks/deploy/fetch.js
+++ tasks/deploy/fetch.js
@@ -1,8 +1,8 @@
-var utils = require('shipit-utils');
-var chalk = require('chalk');
-var mkdirp = require('mkdirp');
-var fs = require('fs');
-var Promise = require('bluebird');
+var utils = require("shipit-utils");
+var chalk = require("chalk");
+var mkdirp = require("mkdirp");
+var fs = require("fs");
+var Promise = require("bluebird");
 
 /**
  * Fetch task.
@@ -11,22 +11,23 @@ var Promise = require('bluebird');
  * - Checkout commit-ish.
  */
 
-module.exports = function (gruntOrShipit) {
-  utils.registerTask(gruntOrShipit, 'deploy:fetch', task);
+module.exports = function(gruntOrShipit) {
+  utils.registerTask(gruntOrShipit, "deploy:fetch", task);
 
   function task() {
     var shipit = utils.getShipit(gruntOrShipit);
 
     if (shipit.config.pullDataDeploy) {
       if (fs.existsSync(shipit.config.workspace)) {
-        return cleanRepository()
+        return checkRepository()
+          .then(cleanRepository)
           .then(resetModified)
           .then(fetchBranch)
           .then(switchBranch)
           .then(pullData)
-          .then(function () {
-            shipit.emit('fetched');
-          })
+          .then(function() {
+            shipit.emit("fetched");
+          });
       } else {
         return createWorkspace()
           .then(initRepository)
@@ -35,9 +36,9 @@ module.exports = function (gruntOrShipit) {
           .then(initData)
           .then(switchBranch)
           .then(pullData)
-          .then(function () {
-            shipit.emit('fetched');
-          })
+          .then(function() {
+            shipit.emit("fetched");
+          });
       }
     } else {
       return createWorkspace()
@@ -49,8 +50,8 @@ module.exports = function (gruntOrShipit) {
         .then(reset)
         .then(merge)
         .then(updateSubmodules)
-        .then(function () {
-          shipit.emit('fetched');
+        .then(function() {
+          shipit.emit("fetched");
         });
     }
 
@@ -59,10 +60,13 @@ module.exports = function (gruntOrShipit) {
      */
 
     function fetchOrigin() {
-      shipit.log('Fetch repository');
-      return shipit.local('git remote add jarvis-shipit ' + shipit.config.repositoryUrl, { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Fetch repository completed.'));
+      shipit.log("Fetch repository");
+      return shipit
+        .local("git remote add jarvis-shipit " + shipit.config.repositoryUrl, {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Fetch repository completed."));
         });
     }
 
@@ -71,10 +75,38 @@ module.exports = function (gruntOrShipit) {
      */
 
     function fetchBranch() {
-      shipit.log('Fetch branchs');
-      return shipit.local('git fetch -p jarvis-shipit', { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Fetch branchs completed.'));
+      shipit.log("Fetch branchs");
+      return shipit
+        .local("git fetch -p jarvis-shipit", { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Fetch branchs completed."));
+        });
+    }
+
+    /**
+     * Check repository status
+     */
+    function checkRepository() {
+      shipit.log("Check repository");
+      return shipit
+        .local("git remote", { cwd: shipit.config.workspace })
+        .then(function(res) {
+          shipit.log(chalk.green("Check repository completed."));
+
+          var remotes = res.stdout ? res.stdout.split(/\s/) : [];
+
+          // not available repository
+          if (remotes[0] !== "jarvis-shipit") {
+            shipit.log("Repository is not available");
+            return shipit
+              .local("rm -rf " + shipit.config.workspace)
+              .then(createWorkspace)
+              .then(initRepository)
+              .then(fetchOrigin)
+              .then(function() {
+                shipit.log(chalk.green("Rebuild repository completed."));
+              });
+          }
         });
     }
 
@@ -83,10 +115,11 @@ module.exports = function (gruntOrShipit) {
      */
 
     function cleanRepository() {
-      shipit.log('Clean repository');
-      return shipit.local('git clean -fd', { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Clean repository completed.'));
+      shipit.log("Clean repository");
+      return shipit
+        .local("git clean -fd", { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Clean repository completed."));
         });
     }
 
@@ -96,10 +129,11 @@ module.exports = function (gruntOrShipit) {
      */
 
     function resetModified() {
-      shipit.log('Reset modified');
-      return shipit.local('git reset --hard', { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Reset modified completed.'));
+      shipit.log("Reset modified");
+      return shipit
+        .local("git reset --hard", { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Reset modified completed."));
         });
     }
 
@@ -109,9 +143,12 @@ module.exports = function (gruntOrShipit) {
 
     function switchBranch() {
       shipit.log('Switch branch to "%s"', shipit.config.branch);
-      return shipit.local('git checkout ' + shipit.config.branch, { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Switch branch completed.'));
+      return shipit
+        .local("git checkout " + shipit.config.branch, {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Switch branch completed."));
         });
     }
 
@@ -121,9 +158,12 @@ module.exports = function (gruntOrShipit) {
 
     function pullData() {
       shipit.log('Pull data from "%s"', shipit.config.branch);
-      return shipit.local('git pull jarvis-shipit ' + shipit.config.branch, { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Pull data completed.'));
+      return shipit
+        .local("git pull jarvis-shipit " + shipit.config.branch, {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Pull data completed."));
         });
     }
 
@@ -133,10 +173,13 @@ module.exports = function (gruntOrShipit) {
      */
 
     function initData() {
-      shipit.log('Init `master` branch');
-      return shipit.local('git pull jarvis-shipit master', { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Init `master` branch completed.'));
+      shipit.log("Init `master` branch");
+      return shipit
+        .local("git pull jarvis-shipit master", {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Init `master` branch completed."));
         });
     }
 
@@ -150,16 +193,16 @@ module.exports = function (gruntOrShipit) {
     function createWorkspace() {
       function create() {
         shipit.log('Create workspace "%s"', shipit.config.workspace);
-        return Promise.promisify(mkdirp)(shipit.config.workspace)
-          .then(function () {
-            shipit.log(chalk.green('Workspace created.'));
-          });
+        return Promise.promisify(mkdirp)(shipit.config.workspace).then(
+          function() {
+            shipit.log(chalk.green("Workspace created."));
+          }
+        );
       }
 
       if (shipit.config.shallowClone) {
         shipit.log('Deleting existing workspace "%s"', shipit.config.workspace);
-        return shipit.local('rm -rf ' + shipit.config.workspace)
-          .then(create);
+        return shipit.local("rm -rf " + shipit.config.workspace).then(create);
       }
     }
 
@@ -168,10 +211,14 @@ module.exports = function (gruntOrShipit) {
      */
 
     function initRepository() {
-      shipit.log('Initialize local repository in "%s"', shipit.config.workspace);
-      return shipit.local('git init', { cwd: shipit.config.workspace })
-        .then(function () {
-          shipit.log(chalk.green('Repository initialized.'));
+      shipit.log(
+        'Initialize local repository in "%s"',
+        shipit.config.workspace
+      );
+      return shipit
+        .local("git init", { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Repository initialized."));
         });
     }
 
@@ -184,17 +231,24 @@ module.exports = function (gruntOrShipit) {
         return Promise.resolve();
       }
 
-      shipit.log('Set custom git config options for "%s"', shipit.config.workspace);
+      shipit.log(
+        'Set custom git config options for "%s"',
+        shipit.config.workspace
+      );
 
-      return Promise.all(Object.keys(shipit.config.gitConfig || {}).map(function (key, gitConfig) {
-        return shipit.local(
-          'git config ' + key + ' "' + shipit.config.gitConfig[key] + '"',
-          { cwd: shipit.config.workspace }
-        );
-      }))
-        .then(function () {
-          shipit.log(chalk.green('Git config set.'));
-        });
+      return Promise.all(
+        Object.keys(shipit.config.gitConfig || {}).map(function(
+          key,
+          gitConfig
+        ) {
+          return shipit.local(
+            "git config " + key + ' "' + shipit.config.gitConfig[key] + '"',
+            { cwd: shipit.config.workspace }
+          );
+        })
+      ).then(function() {
+        shipit.log(chalk.green("Git config set."));
+      });
     }
 
     /**
@@ -202,25 +256,29 @@ module.exports = function (gruntOrShipit) {
      */
 
     function addRemote() {
-      shipit.log('List local remotes.');
+      shipit.log("List local remotes.");
 
       // List remotes.
-      return shipit.local('git remote', { cwd: shipit.config.workspace })
-        .then(function (res) {
+      return shipit
+        .local("git remote", { cwd: shipit.config.workspace })
+        .then(function(res) {
           var remotes = res.stdout ? res.stdout.split(/\s/) : [];
-          var method = remotes.indexOf('shipit') !== -1 ? 'set-url' : 'add';
+          var method = remotes.indexOf("shipit") !== -1 ? "set-url" : "add";
 
-          shipit.log('Update remote "%s" to local repository "%s"',
-            shipit.config.repositoryUrl, shipit.config.workspace);
+          shipit.log(
+            'Update remote "%s" to local repository "%s"',
+            shipit.config.repositoryUrl,
+            shipit.config.workspace
+          );
 
           // Update remote.
           return shipit.local(
-            'git remote ' + method + ' shipit ' + shipit.config.repositoryUrl,
+            "git remote " + method + " shipit " + shipit.config.repositoryUrl,
             { cwd: shipit.config.workspace }
           );
         })
-        .then(function () {
-          shipit.log(chalk.green('Remote updated.'));
+        .then(function() {
+          shipit.log(chalk.green("Remote updated."));
         });
     }
 
@@ -229,20 +287,19 @@ module.exports = function (gruntOrShipit) {
      */
 
     function fetch() {
-      var fetchCommand = 'git fetch shipit --prune';
-      var fetchDepth = shipit.config.shallowClone ? ' --depth=1' : '';
+      var fetchCommand = "git fetch shipit --prune";
+      var fetchDepth = shipit.config.shallowClone ? " --depth=1" : "";
 
       // fetch branches and tags separate to be compatible with git versions < 1.9
-      fetchCommand += fetchDepth + ' && ' + fetchCommand + ' "refs/tags/*:refs/tags/*"';
+      fetchCommand +=
+        fetchDepth + " && " + fetchCommand + ' "refs/tags/*:refs/tags/*"';
 
       shipit.log('Fetching repository "%s"', shipit.config.repositoryUrl);
 
-      return shipit.local(
-        fetchCommand,
-        { cwd: shipit.config.workspace }
-      )
-        .then(function () {
-          shipit.log(chalk.green('Repository fetched.'));
+      return shipit
+        .local(fetchCommand, { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Repository fetched."));
         });
     }
 
@@ -252,12 +309,12 @@ module.exports = function (gruntOrShipit) {
 
     function checkout() {
       shipit.log('Checking out commit-ish "%s"', shipit.config.branch);
-      return shipit.local(
-        'git checkout ' + shipit.config.branch,
-        { cwd: shipit.config.workspace }
-      )
-        .then(function () {
-          shipit.log(chalk.green('Checked out.'));
+      return shipit
+        .local("git checkout " + shipit.config.branch, {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Checked out."));
         });
     }
 
@@ -266,13 +323,11 @@ module.exports = function (gruntOrShipit) {
      */
 
     function reset() {
-      shipit.log('Resetting the working tree');
-      return shipit.local(
-        'git reset --hard HEAD',
-        { cwd: shipit.config.workspace }
-      )
-        .then(function () {
-          shipit.log(chalk.green('Reset working tree.'));
+      shipit.log("Resetting the working tree");
+      return shipit
+        .local("git reset --hard HEAD", { cwd: shipit.config.workspace })
+        .then(function() {
+          shipit.log(chalk.green("Reset working tree."));
         });
     }
 
@@ -281,31 +336,30 @@ module.exports = function (gruntOrShipit) {
      */
 
     function merge() {
-      shipit.log('Testing if commit-ish is a branch.');
+      shipit.log("Testing if commit-ish is a branch.");
 
       // Test if commit-ish is a branch.
-      return shipit.local(
-        'git branch --list ' + shipit.config.branch,
-        { cwd: shipit.config.workspace }
-      )
-        .then(function (res) {
+      return shipit
+        .local("git branch --list " + shipit.config.branch, {
+          cwd: shipit.config.workspace
+        })
+        .then(function(res) {
           var isBranch = !!res.stdout;
 
           if (!isBranch) {
-            shipit.log(chalk.green('No branch, no merge.'));
+            shipit.log(chalk.green("No branch, no merge."));
             return;
           }
 
-          shipit.log('Commit-ish is a branch, merging...');
+          shipit.log("Commit-ish is a branch, merging...");
 
           // Merge branch.
-          return shipit.local(
-            'git merge shipit/' + shipit.config.branch,
-            { cwd: shipit.config.workspace }
-          );
+          return shipit.local("git merge shipit/" + shipit.config.branch, {
+            cwd: shipit.config.workspace
+          });
         })
-        .then(function () {
-          shipit.log(chalk.green('Branch merged.'));
+        .then(function() {
+          shipit.log(chalk.green("Branch merged."));
         });
     }
 
@@ -314,18 +368,17 @@ module.exports = function (gruntOrShipit) {
      */
 
     function updateSubmodules() {
-
       if (!shipit.config.updateSubmodules) {
         return Promise.resolve();
       }
 
-      shipit.log('Updating submodules.');
-      return shipit.local(
-        'git submodule update --init --recursive',
-        { cwd: shipit.config.workspace }
-      )
-        .then(function () {
-          shipit.log(chalk.green('Submodules updated'));
+      shipit.log("Updating submodules.");
+      return shipit
+        .local("git submodule update --init --recursive", {
+          cwd: shipit.config.workspace
+        })
+        .then(function() {
+          shipit.log(chalk.green("Submodules updated"));
         });
     }
   }
